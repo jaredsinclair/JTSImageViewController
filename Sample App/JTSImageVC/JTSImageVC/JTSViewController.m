311f4206208afca93c891ae67b8f5329b02cba30
Jared Sinclair <jaredsinclair.rn@gmail.com>
2015-03-12 21:41:15 -0400

    Update sample app.

--- Sample App/JTSImageVC/JTSImageVC/JTSViewController.m
+++ Sample App/JTSImageVC/JTSImageVC/JTSViewController.m
@@ -11,6 +11,8 @@
 #import "JTSImageViewController.h"
 #import "JTSImageInfo.h"
 
+#define TRY_AN_ANIMATED_GIF 0
+
 @interface JTSViewController ()
 
 @end
@@ -34,7 +36,11 @@
     
     // Create image info
     JTSImageInfo *imageInfo = [[JTSImageInfo alloc] init];
+#if TRY_AN_ANIMATED_GIF == 1
+    imageInfo.imageURL = [NSURL URLWithString:@"http://media.giphy.com/media/O3QpFiN97YjJu/giphy.gif"];
+#else
     imageInfo.image = self.bigImageButton.image;
+#endif
     imageInfo.referenceRect = self.bigImageButton.frame;
     imageInfo.referenceView = self.bigImageButton.superview;
     imageInfo.referenceContentMode = self.bigImageButton.contentMode;
