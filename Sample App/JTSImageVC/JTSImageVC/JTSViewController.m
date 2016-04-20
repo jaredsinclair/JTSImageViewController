e02c67e3fe0611ee1cf92584db3f42755d7533a1
samuel susla <samuel.susla@gmail.com>
2016-04-20 17:34:29 +0200

    make blurry background in sample to more look like Esplorio use case

--- Sample App/JTSImageVC/JTSImageVC/JTSViewController.m
+++ Sample App/JTSImageVC/JTSImageVC/JTSViewController.m
@@ -54,7 +54,7 @@
     JTSImageViewController *imageViewer = [[JTSImageViewController alloc]
                                            initWithImageInfo:imageInfo
                                            mode:JTSImageViewControllerMode_Image
-                                           backgroundStyle:JTSImageViewControllerBackgroundOption_Scaled];
+                                           backgroundStyle:JTSImageViewControllerBackgroundOption_Blurred];
     
     // Present the view controller.
     [imageViewer showFromViewController:self transition:JTSImageViewControllerTransition_FromOriginalPosition];
